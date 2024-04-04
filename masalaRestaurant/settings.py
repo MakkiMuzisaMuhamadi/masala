da6d26befc401f41be15b56221a0bb064a6080fd
unknown <muhamadimuzisamakki@gmail.com>
2024-04-04 07:41:45 -0700

    'h'

--- masalaRestaurant/settings.py
+++ masalaRestaurant/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5jo#p*^t3dwk4vg_uhs@p)rszrsc&@a)-cf($&34jfa%5z3)wq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
